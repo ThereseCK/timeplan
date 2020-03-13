e384f83eb9c196930fa3e5204964d56f1e165596
therese camilla nordnes <59839349+ThereseCK@users.noreply.github.com>
2020-03-13 13:16:08 +0100

    cd

---
+++ model.js
@@ -0,0 +1,199 @@
+const model= {
+    days: ['Mandag', 'Tirsdag', 'Onsdag', 'Torsdag', 'Fredag'],
+    timeSlots: ['09.00 - 10.00 ','10.20 - 11.20','12.00 - 13.00 ','13.20 - 14.30', ],
+    events: [
+        //Modul 1- Mandag 
+        {
+        name: 'Nøkkelkompetanser',
+        nickName: 'NK',
+        time: {
+            day: 0,
+            timeSlot: 0
+        },
+        module: 1,
+        teachers: ['Eskil', 'Geir']
+    },
+    {
+        name: 'Kodeøkt',
+        nickName: 'KT',
+        time: {
+            day: 0,
+            timeSlot: 1
+        },
+        module: 1,
+        teachers: ['Terje']
+    },
+    {
+        name: 'Parprogrammering to og to med mulighet for hjelp',
+        nickName: 'PP',
+        time: {
+            day: 0,
+            timeSlot: 2
+        },
+        module: 1,
+        teachers: ['']
+    },
+    {
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 0,
+            timeSlot: 3
+        },
+        module: 1,
+        teachers: ['Linn']
+    },
+    //Modul1- Tirsdag
+    {
+        name: 'Morramøte med teamet + Fokusert Jobbing alene eller i Team',
+        nickName: 'MM + FJ',
+        time: {
+            day: 1,
+            timeSlot: 0
+        },
+        module: 1,
+        teachers: ['Linn ', 'Kvamme']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 1,
+            timeSlot: 1
+        },
+        module: 1,
+        teachers: ['Linn']
+    },{
+        name: 'Parprogrammering to og to med mulighet for hjelp',
+        nickName: 'PP',
+        time: {
+            day: 1,
+            timeSlot: 2
+        },
+        module: 1,
+        teachers: ['']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 1,
+            timeSlot: 3
+        },
+        module: 1,
+        teachers: ['Linn']
+    },
+    //modul 2 - Onsdag
+    {
+        name: 'Morramøte med teamet + Fokusert Jobbing alene eller i Team',
+        nickName: 'MM + FJ',
+        time: {
+            day: 2,
+            timeSlot: 0
+        },
+        module: 1,
+        teachers: ['Linn ', 'Kvamme']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 2,
+            timeSlot: 1
+        },
+        module: 1,
+        teachers: ['Linn']
+    },{
+        name: 'Parprogrammering to og to med mulighet for hjelp',
+        nickName: 'PP',
+        time: {
+            day: 2,
+            timeSlot: 2
+        },
+        module: 1,
+        teachers: ['']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 2,
+            timeSlot: 3
+        },
+        module: 1,
+        teachers: ['Linn']
+    },
+    // Modul 1- Torsdag
+    {
+        name: 'Morramøte med teamet + Fokusert Jobbing alene eller i Team',
+        nickName: 'MM + FJ',
+        time: {
+            day: 3,
+            timeSlot: 0
+        },
+        module: 1,
+        teachers: ['Linn ', 'Kvamme']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 3,
+            timeSlot: 1
+        },
+        module: 1,
+        teachers: ['Linn']
+    },{
+        name: 'Parprogrammering to og to med mulighet for hjelp',
+        nickName: 'PP',
+        time: {
+            day: 3,
+            timeSlot: 2
+        },
+        module: 1,
+        teachers: ['']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 3,
+            timeSlot: 3
+        },
+        module: 1,
+        teachers: ['Linn']
+    },
+    // Modul 1- Fredag 
+    {
+        name: 'Morramøte med teamet + Fokusert Jobbing alene eller i Team',
+        nickName: 'MM + FJ',
+        time: {
+            day: 4,
+            timeSlot: 0
+        },
+        module: 1,
+        teachers: ['Linn ', 'Kvamme']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 4,
+            timeSlot: 1
+        },
+        module: 1,
+        teachers: ['Linn']
+    },{
+        name: 'Parprogrammering to og to med mulighet for hjelp',
+        nickName: 'PP',
+        time: {
+            day: 4,
+            timeSlot: 2
+        },
+        module: 1,
+        teachers: ['']
+    },{
+        name: 'Fokusert Jobbing alene eller i Team',
+        nickName: 'FJ',
+        time: {
+            day: 4,
+            timeSlot: 3
+        },
+        module: 1,
+        teachers: ['Linn']
+    },
+
+]}
